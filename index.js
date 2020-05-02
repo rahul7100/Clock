00e792d571a60a1b750b5b005bdb4af084171089
rahul7100 <46070054+rahul7100@users.noreply.github.com>
2020-05-02 11:08:54 +0530

    version 1

---
+++ index.js
@@ -0,0 +1,22 @@
+const hourhand = document.querySelector('.hour');
+const minhand = document.querySelector('.min');
+const sechand = document.querySelector('.sec');
+
+function timehand(){
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secdeg = ((seconds/60) * 360) + 90;
+    sechand.style.transform = `rotate(${secdeg}deg)`;
+
+    const mins = now.getMinutes();
+    const mindeg = ((mins/60) * 360) + 90;
+    minhand.style.transform = `rotate(${mindeg}deg)`;
+
+    const hours = now.getHours();
+    const hourdeg = ((hours/12) * 360) + 90;
+    hourhand.style.transform = `rotate(${hourdeg}deg)`;
+}
+
+
+setInterval(timehand, 1000);
